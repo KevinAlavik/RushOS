dbc4c3d4841fe4b1075406daf4e90bafc9e0e009
Kevin Alavik <95900603+KevinAlavik@users.noreply.github.com>
2023-11-19 09:51:34 +0100

    broken tests?

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,43 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
+use crate::vga_buffer::WRITER;
+use crate::vga_buffer::{Color, ColorCode};
 
-static HELLO: &[u8] = b"Hello World!";
+mod vga_buffer;
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
+    println!("Hello World{}", "!");
 
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    #[cfg(test)]
+    test_main();
 
     loop {}
 }
 
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
+}
+
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    WRITER.lock().set_color(Color::Red, Color::Black);
+    println!("{}", info);
     loop {}
 }
